76b2a80c00e42e24053fc453bfb0fee2df89f7ef
Galen Elijah Sabequil <sgalenelijah@gmail.com>
2025-02-13 16:20:31 +0800

    finish!

--- public/index.js
+++ public/index.js
@@ -16,49 +16,18 @@ function showHiddenMessage(){
   surpriseButton.textContent = 'I Love You! 💖';
 }
 
-function showPhotogallery(){
-  photogallery.classList.remove('hidden');
-  photogallery.style.display = 'flex';
-
-}
-
 
 function newPage(){
   window.location.href = "gallery.html";
 }
 
 
-function goBackPage(){
-  window.location.href = "index.html";
-}
 
 
 
 // Add floating hearts
-window.onload = function () {
-  setInterval(() => createFloatingElement('❤️'), 400);
-  setInterval(() => createFloatingElement('💐'), 600);
-};
 
-function createFloatingElement(emoji) {
-  const element = document.createElement('div');
-  element.classList.add('floating-element');
-  element.innerHTML = emoji;
-
-  // Random position anywhere on the page
-  element.style.left = Math.random() * 100 + 'vw';
-  element.style.top = Math.random() * 100 + 'vh';
-
-  // Random animation duration for a natural effect
-  element.style.animationDuration = Math.random() * 3 + 2 + 's';
-
-  document.body.appendChild(element);
-
-  // Remove after animation
-  setTimeout(() => {
-      element.remove();
-  }, 5000);
-}
+  
 
 
 
